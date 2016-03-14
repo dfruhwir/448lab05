29d7a99a993df9d00026b50d2ada44cc79882c14
Dillon Fruhwirth <d487f080@ku.edu>
2016-03-14 00:29:26 -0500

    Initial commit

---
+++ CreateUser.php
@@ -0,0 +1,27 @@
+<?php
+$u = $_POST["u"];
+
+$mysqli = new mysqli("mysql.eecs.ku.edu", "dfruhwir", "sql448", "dfruhwir");
+
+/* check connection */
+if ($mysqli->connect_errno) {
+    printf("Connect failed: %s", $mysqli->connect_error);
+    exit();
+}
+
+if ($u==NULL){
+	printf("User id empty. User not created");
+}
+else {
+	$query = "INSERT INTO Users (user_id) VALUES ('" . $u . "')";
+	if ($mysqli->query($query)) {
+		printf("User created successfully!");
+	}
+	else {
+		printf("User id taken. User not created");
+	}
+}
+
+/* close connection */
+$mysqli->close();
+?>
